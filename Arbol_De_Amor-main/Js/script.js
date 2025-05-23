71ae98f816b506bceba3fd22c5c7f6ea154261d3
JOSE LUIS SANCHEZ <josesanchezconrado@gmail.com>
2025-05-23 11:24:14 -0600

    Update script.js

--- Arbol_De_Amor-main/Js/script.js
+++ Arbol_De_Amor-main/Js/script.js
@@ -70,7 +70,7 @@ function getURLParam(name) {
 function showDedicationText() { //seguidores
   let text = getURLParam('text');
   if (!text) {
-    text = `Para mi mesticita linda MARISA AVILA:\n\nDesde el primer momento supe que eras tú. Tu sonrisa, tu voz, tu forma de ser… todo en ti me hace sentir en casa.\n\nGracias por acompañarme en cada paso, por entenderme incluso en silencio, y por llenar mis días de amor.\n\nTe amo más de lo que las palabras pueden expresar.`;  } else {
+    text = `Para mi mesticita linda MARISA AVILA:\n\nDesde el primer momento supe que eras tú. Tu sonrisa, tu voz, tu forma de ser y tu pasión en todo lo que haces… todo en ti me hace sentir en casa.\n\nGracias por acompañarme en cada paso, por entenderme incluso en silencio, y por llenar mis días de amor.\n\nTe amo más de lo que las palabras pueden expresar.`;  } else {
     text = decodeURIComponent(text).replace(/\\n/g, '\n');
   }
   const container = document.getElementById('dedication-text');
